bcc8e9caa8587bc08ed169019c97a9eaf59414d1
emma-dougherty <doughertyemma@hotmail.com>
2022-05-11 20:13:51 +0100

    first commit

---
+++ src/components/SongList.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+
+
+const SongList = ({songs}) => {
+
+    const songsItems = songs.map((song, index) => {
+        return (
+            <ul>
+                <li>{index + 1}</li>
+                <li>{song["im:name"].label}</li>
+                <li>{song["im:artist"].label}</li>
+            </ul>
+
+        )
+    })
+
+
+    return (
+        <div>
+            <ul>
+                {songsItems}
+            </ul>
+        </div>
+    )
+}
+
+export default SongList
